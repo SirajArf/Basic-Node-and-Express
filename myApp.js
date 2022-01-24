30bb23267e888869a5c83d4b90f9573d0dd86dee
SirajArf <siraj.arfeen1997@gmail.com>
2022-01-24 14:27:41 +0530

    second

--- myApp.js
+++ myApp.js
@@ -1,20 +1,19 @@
-// var express = require('express');
-// var app = express();
-// module.exports = app;
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mySecret = process.env['MESSAGE_STYLE'];
+require('dotenv').config();
 
-app.use(function(req,res){
+
+app.use(function(req,res,next){
 console.log("Hello World");
+next();
 })
 
-app.get('/',function(req, res) {
-    console.log("hello express")
-  res.send("Hello Express");
-})
+// app.get('/',function(req, res) {
+//     console.log("hello express")
+//   res.send("Hello Express");
+// })
 
 app.use(function(req, res, next) {
   console.log(req.method+" "+req.path+" - "+req.ip);
@@ -28,9 +27,9 @@ app.get('/',function(req,res) {
   res.sendFile(__dirname+'/views/index.html')
 });
 
-app.get('/json', function(req,res){
-  res.json({"message": "Hello json"});
-});
+// app.get('/json', function(req,res){
+//   res.json({"message": "Hello json"});
+// });
 
 
 
@@ -39,17 +38,17 @@ app.get('/json', function(req,res){
 
 
 
-// app.use('/json',function(req,res){
-//   if(mySecret = "uppercase"){
-//      console.log("first if")
-//     res.json({"message": "HELLO JSON"});
+app.use('/json',function(req,res){
+  if(process.env.MESSAGE_STYLE = "uppercase"){
+     console.log("first if")
+    res.json({"message": "HELLO JSON"});
    
-//   }else {
-//     res.json({"message": "Hello json"});
-//   }
-//   }
+  }else {
+    res.json({"message": "Hello json"});
+  }
+  }
 
-// );
+);
 
 
 // app.get('/json', function(req,res){
@@ -65,36 +64,36 @@ app.get('/json', function(req,res){
 //   res.json(response);
 
 // })
-// function currentTime(){
-//   return new Date().toString();
-// }
+function currentTime(){
+  return new Date().toString();
+}
 
-// app.get('/now',function(req,res,next){
-//   req.time = currentTime();
-//   next();
-// }, function(req,res){
-//   res.json({"time":req.time});
-// }
-// )
+app.get('/now',function(req,res,next){
+  req.time = currentTime();
+  next();
+}, function(req,res){
+  res.json({"time":req.time});
+}
+)
 
-// app.get("/:word/echo",function(req,res){
-//   res.json({"echo":req.params.word});
-// })
+app.get("/:word/echo",function(req,res){
+  res.json({"echo":req.params.word});
+})
 
-// app.get('/name',function(req,res){
-//   console.log(req.query);
-//   res.json({name: req.query.first+" "+req.query.last})
-// });
+app.get('/name',function(req,res){
+  console.log(req.query);
+  res.json({name: req.query.first+" "+req.query.last})
+});
 
-// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 
-// app.post('/name',function(req,res){
-//   console.log(req.query);
-//   res.json({name: req.body.first+" "+req.body.last})
-// });
+app.post('/name',function(req,res){
+  console.log(req.query);
+  res.json({name: req.body.first+" "+req.body.last})
+});
 
 
 
